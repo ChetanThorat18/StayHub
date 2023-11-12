7e2a8d32b6c6df5e15a679715d888de87affad55
ChetanThorat18 <chetanthorat146@gmail.com>
2023-11-12 09:08:18 +0530

    handled /listings route

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express  = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+
+const path = require("path");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
 
 
 main().then(()=>{
@@ -18,6 +23,11 @@ app.get("/",(req,res)=>{
     res.send("Root directory working..!");
 });
 
+// index route
+app.get("/listings",async (req,res)=>{
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
+});
 
 // start the server
 app.listen(8080,(req,res)=>{
