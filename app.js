b84b45e4ffc44eb261a438b4b973c593a6f9a16a
ChetanThorat18 <chetanthorat146@gmail.com>
2023-11-17 13:01:40 +0530

    Server Side Error Handling

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
 const path = require("path");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -29,10 +31,10 @@ app.get("/",(req,res)=>{
 });
 
 // index route
-app.get("/listings",async (req,res)=>{
+app.get("/listings",wrapAsync(async (req,res)=>{
     const allListings = await Listing.find({});
     res.render("listings/index.ejs",{allListings});
-});
+}));
 
 // New and Create Route for Listing
 // New ---> GET request at /listings/new to collect Form data
@@ -40,42 +42,59 @@ app.get("/listings",async (req,res)=>{
 app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs");
 })
-app.post("/listings",async (req,res)=>{
+app.post("/listings", wrapAsync(async (req,res)=>{
     // let {title,description,image,price,country,location} = req.body;
+    if(!req.body.listing){
+        throw new ExpressError(400,"Send valid data for listing");
+    }
     let listing = req.body.listing;
     // create instance of collection Listing to add it in database
     const newListing = new Listing(listing);
     await newListing.save();
     res.redirect("/listings");
-})
+}))
 
 //Edit and Update Route For Listing
 // Edit ---> GET request at /listings/:id/edit from show.ejs to render edit form
 // Update ---> PUT request at /listings/:id to Update Database
-app.get("/listings/:id/edit",async (req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
     let {id} = req.params;
     const currentListing = await Listing.findById(id);
     res.render("listings/edit.ejs" , {currentListing});
-});
-app.put("/listings/:id",async (req,res)=>{
+}));
+app.put("/listings/:id",wrapAsync(async (req,res)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"Send valid data for listing");
+    }
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
-})
+}))
 
 // Delete Route for Listing --> DELETE request at /listings/:id From show.ejs 
-app.delete("/listings/:id",async (req,res)=>{
+app.delete("/listings/:id",wrapAsync(async (req,res)=>{
     const {id} = req.params;
    let deletedListing = await Listing.findByIdAndDelete(id);
    console.log(deletedListing);
    res.redirect("/listings");
-})
+}))
 
 // Show route( GET request at /listings/:id FROM views/listings/index.ejs )
-app.get("/listings/:id",async (req,res)=>{
+app.get("/listings/:id",wrapAsync(async (req,res)=>{
     let { id } =req.params;
     const currentListing = await Listing.findById(id);
     res.render("listings/show.ejs",{currentListing});
+}))
+
+// Error Handling Middleware for Invalid Route(If req doesn't match to any of above route)
+app.use("*",(req,res,next)=>{
+    next(new ExpressError(404,"Page Not Found !"))
+})
+  
+// Error Handling Middleware
+app.use((err,req,res,next)=>{
+    let {statusCode = 500,message="Something went wrong!"} = err;
+    res.status(statusCode).render("error.ejs" , {message});
 })
 
 // start the server
