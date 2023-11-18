01a3e53e87a15a1ac1b226ff238552ce7de1ca64
ChetanThorat18 <chetanthorat146@gmail.com>
2023-11-18 13:42:56 +0530

    Handling Reviews collection upon deleting listing

--- models/listing.js
+++ models/listing.js
@@ -2,6 +2,7 @@
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
     title : {
@@ -11,8 +12,8 @@ const listingSchema = new Schema({
     description : String,
     image : {
         type:String,
-        default:"https://unsplash.com/photos/man-in-purple-suit-jacket-using-laptop-computer-05XcCfTOzN4" ,
-        set:(v)=> v===""  ? "https://unsplash.com/photos/man-in-purple-suit-jacket-using-laptop-computer-05XcCfTOzN4" : v,
+        default:"https://unsplash.com/phothttps://images.unsplash.com/photo-1626178793926-22b28830aa30?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dos/man-in-purple-suit-jacket-using-laptop-computer-05XcCfTOzN4" ,
+        set:(v)=> v===""  ? "https://https://images.unsplash.com/photo-1626178793926-22b28830aa30?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/photos/man-in-purple-suit-jacket-using-laptop-computer-05XcCfTOzN4" : v,
     },
     price : Number,
     location:String,
@@ -23,6 +24,14 @@ const listingSchema = new Schema({
             ref:"Review"
         }
     ]
+});
+
+// POST middleware to DELETE corresponding reviews from database upon deleting the listing
+listingSchema.post("findOneAndDelete", async(listing)=>{
+    if(listing){
+        await Review.deleteMany({_id : {$in: listing.reviews}})
+    }
+    
 });
 
 // create Collection (model) "Listing"
